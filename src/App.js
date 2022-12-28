0d03e579baa9839340892696037969aa1d63ef56
gurubarathi.k <110015596+gurubarathi@users.noreply.github.com>
2022-12-28 11:56:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import question from "./components/question";
+import { useState } from "react";
 function App() {
+  const [questionIndex,setQuestionIndex]=useState(0);
+  const present_question=question[questionIndex];
+  const[score,setScore]=useState(0);
+  const [results,setResults]=useState(false)
+  const replay=()=>{
+    setQuestionIndex(0);
+    setScore(0);
+    setResults(false);
+  }
+  const get_Choice=(i)=>{
+    if(present_question.answer===i){
+      setScore(score+1);
+    }
+    const next_question=questionIndex+1
+    if(next_question<question.length){
+    setQuestionIndex(questionIndex+1);}
+    else{
+      setResults(true);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="quiz_box">
+      {results?(<><h2>Your Score is={score}</h2>
+      <button onClick={replay} className="play_again_btn">Play Again</button>
+      </>):(
+      <div className="quiz_question">
+        {present_question.question}
+      
+      <div className="quiz_box_options ">
+        <ul className="quiz_ul">
+          {present_question.option.map((option,i)=>
+          {return<li className="quiz_li"
+          onClick={()=>get_Choice(i)} >{option}</li>})
+          }
+        </ul>
+      </div>
+    </div>)}
     </div>
   );
 }
---
+++ src/components/question.js
@@ -0,0 +1,27 @@
+export default[ 
+  {
+    question:'who is CM of Tamilnadu?',
+    option:['Stalin','Kalaingar','Udhayanithi','Edappadi'],
+    answer:0
+   },
+  {
+    question:'Who is PM of India?',
+    option:['Nehru','Modi','Raghul','Vajbhai'],
+    answer:1
+   },
+   {
+    question:'Who is the President of India?',
+    option:['Traupati','Vajbhai','RamnathKovind','VenkaiyaNaidu'],
+    answer:0
+   },
+   {
+    question:'Who is Governer of Tamilnadu',
+    option:['Rosaiya','Tamilisai','SarathPBabu','RN.Ravi'],
+    answer:3
+   },
+   {
+    question:'9%9=?',
+    option:['1','9','0','81'],
+    answer:2
+   }
+]
